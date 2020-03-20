9d1d0887df10115141d7dcc1466dde83dc0cf949
Eduard Jacobs <edoter@gmail.com>
2020-03-20 15:51:26 -0700

    vertion1.2 setup

--- routes/action/userPost/createPost.js
+++
@@ -1,9 +0,0 @@
-module.exports = async ( req, res, db, controller ) => {
-    let form = req.body;
-    try {
-        await controller.post.create( db, form );
-    }
-    catch( error ){
-        res.status( 300 ).json({ error });
-    }
-}
--- routes/index.js
+++
@@ -1,6 +0,0 @@
-const authRoute = require('./authRoute');
-
-module.exports = ( app, db ) => {
-    require('./api-requests/user-api')( app, db, authRoute ),
-    require('./api-requests/post-api')( app, db, authRoute )
-};
